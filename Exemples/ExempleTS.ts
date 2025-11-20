e3ff3866801c9b637026f6cd043a76128eed282c
formateur357 <formateur357@gmail.com>
2025-11-20 12:21:48 +0100

    add exemple

--- Exemples/ExempleTS.ts
+++ Exemples/ExempleTS.ts
@@ -9,3 +9,11 @@ let phrase = "Bonjour " + prenom + nom + " !\n"
 let phrase2 = `Bonjour ${ prenom }${ nom } !\n`
 
 console.log(phrase, phrase2)
+
+let tab: number[] = [ -5, 10, 45 ]
+tab.push(5.5) // valide
+tab.push("bonjour") // invalide
+
+const o = { a: "foo", b: 12, c: "bar"}
+
+const {a, b, c: k} = o // valide
